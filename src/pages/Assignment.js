0a30ae0485a41dd523ce45f66cc6dbcc343505cb
yoel <yoel@macbook-pro.home>
2020-09-28 16:28:38 -0700

    [class] Organize Pages, Add Signup and Login Forms.

---
+++ src/pages/Assignment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Assignment = (props) => {
+    return (
+        <div>
+          <h1>This is the Assignment Page.</h1>
+        </div>
+    )
+}
+
+export default Assignment
