5a19a18481403cb7243bf19efefa96178d387574
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-09-30 23:24:27 -0600

    classes axios call skeleton

--- src/pages/Class.js
+++ src/pages/Class.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ClassCard from '../components/ClassCard';
 import { Box, ResponsiveContext, Grid } from 'grommet';
 import AddClassModal from '../components/AddClassModal';
+import axios from 'axios'
 
 const Class = props => {
+	const authorizationHeader = {
+		'Authorization': `Bearer ${props.token}`
+	  }
+	
+	useEffect(() => {
+		axios
+		  .get(`http://localhost:8000/api/classroom/${props.currentUser.user_id}`, authorizationHeader)
+		  .then(res => {
+			console.log('Here is the class data: ', res.data)
+		  })
+		  .catch(err =>
+			console.log(err, "You've hit an error in the axios call for classes")
+		  )
+	  }, [props.token, props.currentUser])
+
 	return (
 		<>
 			<Box direction='row' justify='center' margin={{ top: 'large' }}>
