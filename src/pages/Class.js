03edcb93b6bdc55700858c8473fe2d93faeddd4e
Chamon562 <Chamon562@gmail.com>
2020-10-01 09:09:15 -0700

    updating to pull recent changes if any

--- src/pages/Class.js
+++ src/pages/Class.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ClassCard from '../components/ClassCard';
 import { Box, ResponsiveContext, Grid } from 'grommet';
 import AddClassModal from '../components/AddClassModal';
