72664f5a206e8f15fd5d3757ede5ab72f2ca2194
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-10-02 03:12:59 -0600

    integrate assignmentsclasses modal with backend

--- src/components/Assignments/AssignmentList.js
+++ src/components/Assignments/AssignmentList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -12,6 +12,7 @@ import Typography from '@material-ui/core/Typography';
 import Class from '@material-ui/icons/Class';
 import Add from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -26,18 +27,33 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
-function generate(element) {
-	return [0, 1, 2].map(value =>
-		React.cloneElement(element, {
-			key: value,
-		})
-	);
-}
 
 const AssignmentList = props => {
 	const classes = useStyles();
-	const [dense, setDense] = React.useState(false);
-	const [secondary, setSecondary] = React.useState(false);
+	const [dense, setDense] = useState(false);
+	const [secondary, setSecondary] = useState(false);
+	const [classrooms, setClassrooms] = useState([])
+	
+	const authorizationHeader = {
+		headers: {'Authorization': `Bearer ${props.token}`}
+	}
+
+	useEffect(() => {
+		axios
+		// path to get all classrooms associated with assignment
+		  .get('http://localhost:8000/api/classrooms-assignments/', {
+			assignment: props.assignment.id
+			}
+			,authorizationHeader)
+		  .then(res => {
+			setClassrooms(res.data)
+			console.log('Here is the class data: ', res.data)
+		  })
+		  .catch(err =>
+			console.log(err, "You've hit an error in the axios call for assignments")
+		  )
+	  }, [props.token, props.currentUser])
+
 
 	return (
 		<>
@@ -48,24 +64,24 @@ const AssignmentList = props => {
 					</Typography>
 					<div className={classes.demo}>
 						<List dense={dense}>
-							{generate(
-								<ListItem>
-									<ListItemAvatar>
-										<Avatar>
-											<Class />
-										</Avatar>
-									</ListItemAvatar>
-									<ListItemText
-										primary='Single-line item'
-										secondary={secondary ? 'Secondary text' : null}
-									/>
-									<ListItemSecondaryAction>
-										<IconButton edge='end' aria-label='delete'>
-											<DeleteIcon />
-										</IconButton>
-									</ListItemSecondaryAction>
-								</ListItem>
-							)}
+							{classrooms.map((classroom, i) => {
+								return(<ListItem key={i}>
+											<ListItemAvatar>
+												<Avatar>
+													<Class />
+												</Avatar>
+											</ListItemAvatar>
+											<ListItemText
+												primary={classroom.name}
+												secondary={classroom.gradeLevel}
+											/>
+											<ListItemSecondaryAction>
+												<IconButton edge='end' aria-label='delete'>
+													<DeleteIcon />
+												</IconButton>
+											</ListItemSecondaryAction>
+										</ListItem>)
+							})}
 						</List>
 					</div>
 				</Grid>
@@ -75,7 +91,7 @@ const AssignmentList = props => {
 					</Typography>
 					<div className={classes.demo}>
 						<List dense={dense}>
-							{generate(
+							{/* {generate(
 								<ListItem>
 									<ListItemAvatar>
 										<Avatar>
@@ -92,7 +108,7 @@ const AssignmentList = props => {
 										</IconButton>
 									</ListItemSecondaryAction>
 								</ListItem>
-							)}
+							)} */}
 						</List>
 					</div>
 				</Grid>
